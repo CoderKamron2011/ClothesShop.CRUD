ddc0be353b9dd83bdf58b9264adf29c7fb1a4871
CoderKamron2011 <hasanovaruxshona774@gmail.com>
2024-05-27 11:51:21 +0500

    Logging Impliment

--- FlowerShop.CRUD/Brokers/Logging/IloggingBroker.cs
+++ FlowerShop.CRUD/Brokers/Logging/IloggingBroker.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace FlowerShop.CRUD.Brokers.Logging
 {
-    internal class IloggingBroker
+    internal interface ILoggingBroker
     {
+        void LogInformation(string message);
+        void LogError(string userMessage);
     }
 }
